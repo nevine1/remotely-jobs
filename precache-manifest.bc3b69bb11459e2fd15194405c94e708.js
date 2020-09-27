125a54ad6e9bd453ae03e61b1ef90a83aaed96b4
sidiDev <sididev3@gmail.com>
2020-09-27 15:07:17 +0000

    Updates

---
+++ precache-manifest.bc3b69bb11459e2fd15194405c94e708.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ae8c467051e7bb77c21827b05a4c1e",
+    "url": "/remotely-jobs/index.html"
+  },
+  {
+    "revision": "12c52c2aa5c63031abb1",
+    "url": "/remotely-jobs/static/css/main.26ae9f44.chunk.css"
+  },
+  {
+    "revision": "083640d12a9503e1b79c",
+    "url": "/remotely-jobs/static/js/2.a5ced70c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/remotely-jobs/static/js/2.a5ced70c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12c52c2aa5c63031abb1",
+    "url": "/remotely-jobs/static/js/main.92a77b36.chunk.js"
+  },
+  {
+    "revision": "b41b5a0bec87eb53faec",
+    "url": "/remotely-jobs/static/js/runtime-main.bbaac2b6.js"
+  }
+]);
